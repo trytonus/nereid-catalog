e8c8e7e0cafa56d96c538e032570b7a001094c4c
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2013-01-29 15:45:33 +0530

    Upgrade tests to nereid 2.4.0.8 #673
    Testing was revamped in nereid 2.4.0.7 to work with unittest fixtures
    and the tryton test loader. This patch migrates the module to use the
    new testing and also use the travis CI engine to test the module.
    
    Makes module depend on version 2.4.0.8 of nereid which fixes an
    important issue with site-maps which is needed for this module to
    generate sitemaps effectively at runtime.

--- __init__.py
+++ __init__.py
@@ -3,10 +3,9 @@
     
     nereid_catalog
     
-    :copyright: (c) 2010-2012 by Openlabs Technologies & Consulting (P) Ltd.
+    :copyright: (c) 2010-2013 by Openlabs Technologies & Consulting (P) Ltd.
     :license: GPLv3, see LICENSE for more details
     
 '''
 
 from .product import *
-from .testing import *
