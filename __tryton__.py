57bc20cba847aa38d5d36b297e84a901004a9836
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2013-08-16 10:13:51 +0530

    Bump version to 2.4.0.3

--- __tryton__.py
+++ __tryton__.py
@@ -2,7 +2,7 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'Nereid Catalog',
-    'version': '2.4.0.2',
+    'version': '2.4.0.3',
     'author': 'Openlabs Technologies & Consulting (P) LTD',
     'email': 'info@openlabs.co.in',
     'website': 'http://www.openlabs.co.in/',
