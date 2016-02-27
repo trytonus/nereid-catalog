a65606da03eca885b90749a415cd1502d8c8e42b
Mathias Behrle <mathiasb@m9s.biz>
2016-02-27 13:53:10 +0100

    Fixing W503 in setup.py.

--- setup.py
+++ setup.py
@@ -94,9 +94,9 @@ setup(
     },
     package_data={
         'trytond.modules.nereid_catalog':
-            info.get('xml', [])
-            + ['tryton.cfg', 'locale/*.po', 'tests/*.rst', 'view/*.xml']
-            + ['i18n/*.pot', 'i18n/pt_BR/LC_MESSAGES/*'],
+        info.get('xml', []) +
+        ['tryton.cfg', 'locale/*.po', 'tests/*.rst', 'view/*.xml'] +
+        ['i18n/*.pot', 'i18n/pt_BR/LC_MESSAGES/*'],
     },
     license='BSD',
     install_requires=requires,
